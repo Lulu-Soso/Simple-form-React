12f25c441d75a6b5d16fd197be117938f7acaee9
Lulu-Soso <lucas.sokha@gmail.com>
2023-11-20 00:24:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
+import handIMG from "./images/hand.jpg";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  console.log("errors", errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {/* Form Column */}
+        <div className="column">
+          <div className="left-half">
+            <h1>Contact Us</h1>
+            <p>We're open for any suggestion or just to have a chat.</p>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* Name */}
+              <p className="input-label">Name *</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  maxLength: 80,
+                })}
+              />
+              {errors.name && (
+                <span className="error">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length of name is 80 char."}
+                </span>
+              )}
+
+              {/* Email */}
+              <p className="input-label">Email *</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <span className="error">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid Email Address."}
+                </span>
+              )}
+
+              {/* Phone number */}
+              <p className="input-label">Phone</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Phone #"
+                {...register("phoneNumber", {
+                  pattern:
+                    /^(?:(?:(?:\+|00)33[\s.-]?)|0)[1-9](?:(?:[\s.-]?[0-9]{2}){4})$/,
+                })}
+              />
+              {errors.phoneNumber && (
+                <span className="error">Invalid Phone Number</span>
+              )}
+
+              {/* Message */}
+              <p className="input-label">Message *</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Write your message"
+                {...register("message", { required: true })}
+              />
+              {errors.message && (
+                <span className="error">This field is required.</span>
+              )}
+
+              {/* Submit */}
+              <div>
+                <input className="submit" type="submit" value="Send Message" />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Image Column */}
+        <div className="column">
+          <img src={handIMG} alt="hand" />
+        </div>
+      </div>
     </div>
   );
 }
